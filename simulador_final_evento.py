9eee6bb0f0b142aa94a41482aca8e5a319110654
Joao Melo <joaovicmelo@gmail.com>
2025-08-15 15:37:53 -0300

    Ajuste na cor do resumo/DRE

--- simulador_final_evento.py
+++ simulador_final_evento.py
@@ -313,7 +313,7 @@ if VISAO == "DRE's Comparativas":
         ]
     }
     cor_customizada_2026 = {
-        'Receita Bruta (pré CBS)': '#333333',  # Ouro
+        'Receita Bruta (pré CBS)': '#a64d79',  # Ouro
         'Lucro Líquido': '#4682B4',  # Verde
         'Margem de Lucro': '#6B8E23',  # Azul claro
     }
@@ -365,7 +365,7 @@ if VISAO == "DRE's Comparativas":
 
 
     cor_customizada_c2 = {
-        'Receita Bruta (pré CBS)': '#333333',  # Ouro
+        'Receita Bruta (pré CBS)': '#a64d79',  # Ouro
     }
 
     dados_rc3 = {
